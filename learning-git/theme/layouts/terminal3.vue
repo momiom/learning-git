75a4fb0775f90c80a9ccce7f778b022a9bc02e87
momiom <kazuki.k.bl8+github.momio@gmail.com>
2022-12-18 17:09:43 +0900

    layout を追加

---
+++ learning-git/theme/layouts/terminal3.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { handleBackground } from '../layoutHelper'
+
+const props = defineProps({
+  background: {
+    default: '',
+  },
+})
+
+const style = computed(() => handleBackground(props.background, true))
+</script>
+
+<template>
+  <div class="slidev-layout default terminal" :style="style">
+    <slot />
+    <div class="grid grid-cols-[1fr,1fr] gap-4 h-full">
+      <div>
+        <slot name="left" />
+      </div>
+      <div>
+        <slot name="right" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ learning-git/theme/layouts/two-col-2.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { handleBackground } from '../layoutHelper'
+
+const props = defineProps({
+  background: {
+    default: '',
+  },
+})
+
+const style = computed(() => handleBackground(props.background, true))
+</script>
+
+<template>
+  <div class="slidev-layout default tow-col" :style="style">
+    <slot />
+    <div class="grid grid-cols-[1.5fr,1fr] gap-4">
+      <div>
+        <slot name="left" />
+      </div>
+      <div>
+        <slot name="right" />
+      </div>
+    </div>
+  </div>
+</template>
